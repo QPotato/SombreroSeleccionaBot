5edb34423ed70ca7da08dfdd30663efa97404663
QPotato <federicobadaloni@hotmail.com>
2019-06-25 15:00:52 -0300

    pedidos de la banda

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,5 @@
 import * as TelegramBot from 'node-telegram-bot-api';
 
-const welcome_message = 'Bienvenid@ al grupo!\nPara oficializar el ingreso registrate acá: https://forms.gle/333rK4PMqGUpURSbA';
-
 const init = async () => {
     // Create the telegram bot
     if (process.env.TELEGRAM === undefined)
@@ -9,8 +7,13 @@ const init = async () => {
     const telegramtoken = process.env.TELEGRAM;
     const bot = new TelegramBot(telegramtoken, { polling: true });
     bot.on('message', (msg: TelegramBot.Message) => {
-        if ('new_chat_members' in msg)
+        if (msg.new_chat_members !== undefined && !msg.new_chat_members[0].is_bot) {
+            const welcome_message = `Bienvenidx al grupo @${msg.new_chat_members[0].username}!\nPara oficializar el ingreso registrate acá: https://forms.gle/333rK4PMqGUpURSbA`;
             bot.sendMessage(msg.chat.id, welcome_message);
+        }
+    });
+    bot.onText(/\/lista/, (msg) => {
+        bot.sendMessage(msg.chat.id, 'https://docs.google.com/forms/d/e/1FAIpQLSdk96jjuIbx5qa-9wWR3HMLynBgqwaiJeqeHeChv3opbUiGZA/viewanalytics')
     });
 }
 
